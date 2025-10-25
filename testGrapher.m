83e13fe7bae6a701eac46f04c91b5693c55c1142
PMurphyB <pmurphyguitar@gmail.com>
2025-10-25 16:59:42 -0600

    Added subplots

--- testGrapher.m
+++ testGrapher.m
@@ -1,11 +1,13 @@
 clc, clearvars, clear all
 
 path = '/Users/Blanchards1/Documents/FormulaSim/new-cornering-simulation';
-allDataPath = fullfile(path, 'R20_infoTable.csv');
+allDataPath = fullfile(path, 'R20_filtered_table.csv');
+extraDataPath = fullfile(path, 'R20_infoTable.csv');
 rangeDataPath = fullfile(path, 'R20_ranges.csv');
 
 allData = readtable(allDataPath);
 rangeData = readtable(rangeDataPath);
+extraData = readtable(extraDataPath);
 
 validTimes = rangeData.ElapsedTime;
 
@@ -16,6 +18,40 @@ for i = 1:size(timeRanges, 1)
     mask = mask | (allData.ElapsedTime >= timeRanges(i, 1) & allData.ElapsedTime <= timeRanges(i, 2));
 end
 
-filtereAll = allData(mask, :);
+filteredAll = allData(mask, :);
 disp("Finished Masking");
 
+filteredAll = sortrows(filteredAll, "ElapsedTime");
+
+figure('Position', [200 200 900 600]);
+
+t = filteredAll.ElapsedTime;
+
+subplot(3, 1, 1);
+plot(t, filteredAll.NormalForce, 'b');
+xlabel('Elapsed Time (s)');
+ylabel('Normal Force (N)');
+title('Normal Force vs. Time');
+grid on;
+
+subplot(3,1,2);
+plot(t, filteredAll.SlipAngle, 'r');
+xlabel('Elapsed Time (s)');
+ylabel('Slip Angle (deg)');
+title('Slip Angle vs. Time');
+grid on;
+
+subplot(3,1,3);
+plot(t, filteredAll.TirePressure, 'Color', [0.1 0.6 0.1], 'LineWidth', 1.5);
+xlabel('Elapsed Time (s)');
+ylabel('Tire Pressure (PSI)');
+title('Tire Pressure vs. Time');
+grid on;
+
+
+sgtitle('Parameter Behavior in Selected Ranges (R20 Test)');
+
+
+%outFile = fullfile(basePath, 'R20_infoTable_filteredByRanges.csv');
+%writetable(filteredAll, outFile);
+%disp("Filtered data saved to: " + outFile);
