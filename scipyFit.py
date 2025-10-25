ffdcba37a53ef3a4f9e8660872f2d91ad2ba2408
Andrew Sautel <152945937+violinBhoy@users.noreply.github.com>
2025-10-25 16:38:10 -0600

    Changed values that should be held at 0 to be 0 in pacejka function

--- scipyFit.py
+++ scipyFit.py
@@ -8,9 +8,9 @@ def pacejka(P, L, FZ, IA, alpha):
     #P = [250, 1.4, 2.4, -0.25, 3, -0.1, -1.5, 0, 0, -30.5, 1.15, 1, 0, 0, -0.128, 0, 0, 0, 1.43];
     #L = [1, 1, 1, 1, 1, 1, 1, 1];
     dfz = (FZ - P[0]) / P[0]
-    Svy = FZ * (P[15] + P[16]*dfz + (P[17] + P[18]*dfz)*IA) * L[7] * L[4]
-    Ey = (P[5] + P[6]*dfz) * (1 - (P[7] + P[8]*IA) * np.sign(alpha)) * L[6]
-    Shy = (P[12] + P[13]*dfz + P[14]*IA) * L[5]
+    Svy = FZ * (0 + 0*dfz + (0 + P[18]*dfz)*IA) * L[7] * L[4]
+    Ey = (P[5] + P[6]*dfz) * (1 - (0 + 0*IA) * np.sign(alpha)) * L[6]
+    Shy = (0 + 0*dfz + P[14]*IA) * L[5]
     alphaY = alpha + Shy
     Cy = P[1] * L[1]
     Dy = FZ * (P[2] + P[3]*dfz) * (1 - P[4]*IA**2) * L[0]
@@ -25,7 +25,7 @@ def pacejka(P, L, FZ, IA, alpha):
 #make them as close to each other as possible
 def diff(P, L, FZ, IA, alpha, Ty_measured):
     return pacejka(P, L, FZ, IA, alpha) - Ty_measured
-
+# 15 16 17
 #load the data
 df = pd.read_csv("C:/Users/ajsau/Documents/formula/corneringSim/cornering-simulation/LCO_infoTable.csv")
 P = np.array([250, 1.4, 2.4, -0.25, 3, -0.1, -1.5, 0, 0, -30.5, 1.15, 1, 0, 0, -0.128, 0, 0, 0, 1.43])
