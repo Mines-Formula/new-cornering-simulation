592f2543a5a5a21fccf65e5149945a827f7d153c
Andrew Sautel <152945937+violinBhoy@users.noreply.github.com>
2025-10-25 16:45:57 -0600

    changed P arrays in pacejka to be P arrays declarinig whether it is dependent on camber or not.

--- scipyFit.py
+++ scipyFit.py
@@ -7,15 +7,15 @@ def pacejka(P, L, FZ, IA, alpha):
     #alpha is slip angle
     #P = [250, 1.4, 2.4, -0.25, 3, -0.1, -1.5, 0, 0, -30.5, 1.15, 1, 0, 0, -0.128, 0, 0, 0, 1.43];
     #L = [1, 1, 1, 1, 1, 1, 1, 1];
-    dfz = (FZ - P[0]) / P[0]
-    Svy = FZ * (0 + 0*dfz + (0 + P[18]*dfz)*IA) * L[7] * L[4]
-    Ey = (P[5] + P[6]*dfz) * (1 - (0 + 0*IA) * np.sign(alpha)) * L[6]
-    Shy = (0 + 0*dfz + P[14]*IA) * L[5]
+    dfz = (FZ - P_noCamber[0]) / P_noCamber[0]
+    Svy = FZ * (0 + 0*dfz + (0 + P_camber[6]*dfz)*IA) * L[7] * L[4]
+    Ey = (P_noCamber[4] + P_noCamber[5]*dfz) * (1 - (0 + 0*IA) * np.sign(alpha)) * L[6]
+    Shy = (0 + 0*dfz + P_camber[3]*IA) * L[5]
     alphaY = alpha + Shy
-    Cy = P[1] * L[1]
-    Dy = FZ * (P[2] + P[3]*dfz) * (1 - P[4]*IA**2) * L[0]
-    x1 = 2 * np.arctan(FZ / (P[10]*P[0]*L[2]))
-    x2 = P[9]*P[0]*np.sin(x1) * (1 - P[11]*np.abs(IA)) * L[3] * L[4]
+    Cy = P_noCamber[1] * L[1]
+    Dy = FZ * (P_noCamber[2] + P_noCamber[3]*dfz) * (1 - P_camber[0]*IA**2) * L[0]
+    x1 = 2 * np.arctan(FZ / (P_noCamber[9]*P_noCamber[0]*L[2]))
+    x2 = P_noCamber[8]*P_noCamber[0]*np.sin(x1) * (1 - P_camber[1]*np.abs(IA)) * L[3] * L[4]
     By = x2 / (Cy * Dy)
     x3 = By * alphaY
     FY = Dy * np.sin(Cy * np.arctan(x3 - Ey * (x3 - np.arctan(x3)))) + Svy
@@ -25,7 +25,7 @@ def pacejka(P, L, FZ, IA, alpha):
 #make them as close to each other as possible
 def diff(P, L, FZ, IA, alpha, Ty_measured):
     return pacejka(P, L, FZ, IA, alpha) - Ty_measured
-# 15 16 17
+
 #load the data
 df = pd.read_csv("C:/Users/ajsau/Documents/formula/corneringSim/cornering-simulation/LCO_infoTable.csv")
 P = np.array([250, 1.4, 2.4, -0.25, 3, -0.1, -1.5, 0, 0, -30.5, 1.15, 1, 0, 0, -0.128, 0, 0, 0, 1.43])
