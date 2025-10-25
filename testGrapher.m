e5e06038ea69190d8669b00028d8542c0720267e
PMurphyB <pmurphyguitar@gmail.com>
2025-10-25 16:38:20 -0600

    Added mask for data

--- testGrapher.m
+++ testGrapher.m
@@ -9,6 +9,10 @@ rangeData = readtable(rangeDataPath);
 
 validTimes = rangeData.ElapsedTime;
 
-ranges = [13.0, 91.86; 239.92, 460.94; 716.424, 903.17];
+timeRanges = [13.0, 91.86; 239.92, 460.94; 716.424, 903.17];
 
 mask = false(height(allData), 1);
+for i = 1:size(timeRanges, 1)
+    mask = mask | (allData.ElapsedTime >= timeRanges(i, 1) & allData.ElapsedTime <= timeRanges(i, 2));
+end
+
