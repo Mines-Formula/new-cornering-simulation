111bd343dd7b484a880fa06ec9247d164423d089
PMurphyB <pmurphyguitar@gmail.com>
2025-10-25 16:17:20 -0600

    Added binned values

--- calculator.m
+++ calculator.m
@@ -28,19 +28,32 @@ FZ_binned = round(FZ / 50) * 50;
 [alphaSorted, idx] = sort(alpha);
 FY_exp_sorted = FY_exp(idx);
 FY_calc_sorted = FY(idx);
+FZ_binned_sorted = FZ_binned(idx);
 
+uniqueBins = unique(FZ_binned_sorted);
+numBins = numel(uniqueBins);
+colors = turbo(numBins);
 
 figure;
-scatter(alphaSorted, FY_exp_sorted, 10, 'r', 'filled', 'DisplayName', 'Experimental FY');
 hold on;
-scatter(alphaSorted, FY_calc_sorted, 10, 'b', 'filled', 'DisplayName', 'Calculated FY (Pacejka)');
+for i = 1:numBins
+    binMask = FZ_binned_sorted == uniqueBins(i);
+    scatter(alphaSorted(binMask), FY_exp_sorted(binMask), 10, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'DisplayName', sprintf('Exp FY - FZ ≈ %d N', uniqueBins(i)));
+    scatter(alphaSorted(binMask), FY_calc_sorted(binMask), 10, 'MarkerEdgeColor', colors(i,:), 'MarkerFaceColor', 'none', 'DisplayName', sprintf('Calc FY - FZ ≈ %d N', uniqueBins(i)));
+end
 hold off;
 
 grid on;
 xlabel('Slip Angle (deg)');
 ylabel('Lateral Force FY (N)');
-title('Experimental vs. Calculated Lateral Force (Pacejka Model)');
-legend('Location', 'best');
+title('Experimental vs. Calculated Lateral Force Colored by Normal Force');
+legend('Location', 'bestoutside');
 
-rmse = sqrt(mean((FY - FY_exp).^2));
+colormap(colors);
+c = colorbar;
+c.Ticks = linspace(0, 1, numBins);
+c.TickLabels = string(uniqueBins);
+c.Label.String = 'Normal Force (N, binned to nearest 50)';
+
+rmse = sqrt(mean((FY_calc - FY_exp).^2));
 disp(['RMSE between experimental and calculated FY: ', num2str(rmse, '%.3f'), ' N']);
