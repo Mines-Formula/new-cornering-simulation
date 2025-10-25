d3458c5733481958eb81cc2517a3a5dfe0704197
PMurphyB <pmurphyguitar@gmail.com>
2025-10-25 11:09:20 -0600

    Added FY result

--- calculator.m
+++ calculator.m
@@ -3,3 +3,17 @@ clc, clearvars, clear all
 dataPath = '/Users/Blanchards1/Documents/FormulaSim/new-cornering-simulation/R20_infoTable.csv';
 data = readtable(dataPath);
 
+FZ = data.NormalForce;
+IA = data.InclinationAngle;
+alpha = data.SlipAngle;
+
+P = [250, 1.4, 2.4, -0.25, 3, -0.1, -1.5, 0, 0, -30.5, 1.15, 1, 0, 0, -0.128, 0, 0, 0, 1.43];
+L = [1, 1, 1, 1, 1, 1, 1, 1];
+
+FY = pacejka(P, L, FZ, IA, alpha);
+
+data.FYPacejka = FY;
+
+outFile = fullfile('/Users/Blanchards1/Documents/FormulaSim/new-cornering-simulation', 'R20_with_Pacejka.csv');
+writetable(resultTable, outFile);
+disp("Finished");
