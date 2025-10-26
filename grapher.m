377a1b4882057505e2bee36beaf3c263fb19adfd
PMurphyB <pmurphyguitar@gmail.com>
2025-10-26 15:43:36 -0600

    Removed old graphs, added time constraints

--- grapher.m
+++ grapher.m
@@ -8,6 +8,7 @@ IA = data.InclinationAngle;
 alpha = data.SlipAngle;
 FY_exp = data.LateralForce;
 FY = data.FY;
+time = data.ElapsedTime;
 
 FZ_binned = round(FZ / 50) * 50;
 
@@ -17,30 +18,11 @@ FZ_binned = round(FZ / 50) * 50;
 FY_exp_sorted = FY_exp(idx);
 FY_calc_sorted = FY(idx);
 FZ_binned_sorted = FZ_binned(idx);
+timeSorted = time(idx);
 
 uniqueBins = unique(FZ_binned_sorted);
 numBins = numel(uniqueBins);
 colors = turbo(numBins);
 
-figure;
-hold on;
-for i = 1:numBins
-    binMask = FZ_binned_sorted == uniqueBins(i);
-    scatter(alphaSorted(binMask), FY_exp_sorted(binMask), 10, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'DisplayName', sprintf('Exp FY - FZ ≈ %d', uniqueBins(i)));
-    %scatter(alphaSorted(binMask), FY_calc_sorted(binMask), 10, 'MarkerEdgeColor', colors(i,:), 'MarkerFaceColor', 'none', 'DisplayName', sprintf('Calc FY - FZ ≈ %d', uniqueBins(i)));
-end
-hold off;
+timeWindows = [13.0, 91.86; 239.92, 460.94; 716.424, 903.17];
 
-grid on;
-xlabel('Slip Angle (deg)');
-ylabel('Lateral Force FY');
-title('Experimental vs. Calculated Lateral Force Colored by Normal Force');
-legend('Location', 'bestoutside');
-
-colormap(colors);
-c = colorbar;
-c.Ticks = linspace(0, 1, numBins);
-c.TickLabels = string(uniqueBins);
-
-rmse = sqrt(mean((FY - FY_exp).^2));
-disp(['RMSE between experimental and calculated FY: ', num2str(rmse, '%.3f')]);
