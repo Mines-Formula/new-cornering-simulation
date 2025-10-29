9e1406253e884ef9ab976d2f2379d8c898ccdcfd
PMurphyB <pmurphyguitar@gmail.com>
2025-10-29 16:14:34 -0600

    Went back to multiple graphs for the different times

--- grapher.m
+++ grapher.m
@@ -34,27 +34,60 @@ uniqueBins = unique(FZ_binned_sorted);
 numBins = numel(uniqueBins);
 colors = turbo(numBins);
 
-figure;
-hold on;
+timeWindows = [12.64, 91.83; 239.67, 461.73; 715.954, 904.31];
 
-for i = 1:numBins
+for w = 1:size(timeWindows, 1)
+    tStart = timeWindows(w, 1);
+    tEnd = timeWindows(w, 2);
 
-    binMask = FZ_binned_sorted == uniqueBins(i);
-    scatter(alphaSorted(binMask), FY_exp_sorted(binMask), 10, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'DisplayName', sprintf('Exp FY - FZ ≈ %d', uniqueBins(i)));
-    %scatter(alphaSorted(binMask), FY_calc_sorted(binMask), 10, 'MarkerEdgeColor', colors(i,:), 'MarkerFaceColor', 'none', 'DisplayName', sprintf('Calc FY - FZ ≈ %d', uniqueBins(i)));
+    timeMask = (timeSorted >= tStart) & (timeSorted <= tEnd);
+
+    figure;
+    hold on;
+
+    for i = 1:numBins
+        binMask = (FZ_binned_sorted == uniqueBins(i)) & timeMask;
+        if any(binMask)
+            scatter(alphaSorted(binMask), FY_exp_sorted(binMask), 10, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'DisplayName', sprintf('Exp FY - FZ ≈ %d', uniqueBins(i)));
+        end
+    end
+
+    hold off;
+    grid on;
+    xlabel('Slip Angle');
+    ylabel('Lateral Force FY');
+
+    colormap(colors);
+    c = colorbar;
+    c.Ticks = linspace(0, 1, numBins);
+    c.TickLabels = string(uniqueBins);
+
 end
-hold off;
 
-grid on;
-xlabel('Slip Angle (deg)');
-ylabel('Lateral Force FY');
-title('Experimental vs. Calculated Lateral Force Colored by Normal Force');
-legend('Location', 'bestoutside');
 
-colormap(colors);
-c = colorbar;
-c.Ticks = linspace(0, 1, numBins);
-c.TickLabels = string(uniqueBins);
+
+%% Old graphing section
+%figure;
+%hold on;
+
+%for i = 1:numBins
+
+    %binMask = FZ_binned_sorted == uniqueBins(i);
+    %scatter(alphaSorted(binMask), FY_exp_sorted(binMask), 10, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'DisplayName', sprintf('Exp FY - FZ ≈ %d', uniqueBins(i)));
+    %scatter(alphaSorted(binMask), FY_calc_sorted(binMask), 10, 'MarkerEdgeColor', colors(i,:), 'MarkerFaceColor', 'none', 'DisplayName', sprintf('Calc FY - FZ ≈ %d', uniqueBins(i)));
+%end
+%hold off;
+
+%grid on;
+%xlabel('Slip Angle (deg)');
+%ylabel('Lateral Force FY');
+%title('Experimental vs. Calculated Lateral Force Colored by Normal Force');
+%legend('Location', 'bestoutside');
+
+%colormap(colors);
+%c = colorbar;
+%c.Ticks = linspace(0, 1, numBins);
+%c.TickLabels = string(uniqueBins);
 
 %rmse = sqrt(mean((FY - FY_exp).^2));
 %disp(['RMSE between experimental and calculated FY: ', num2str(rmse, '%.3f')]);
