716993c93b822bd1864ad06ee159b05fdb6e9118
PMurphyB <pmurphyguitar@gmail.com>
2025-10-29 15:02:52 -0600

    Removed Time Windows and old graphing method

--- grapher.m
+++ grapher.m
@@ -1,13 +1,13 @@
 clc, clearvars, clear all
 
-dataPath = '/Users/Blanchards1/Documents/FormulaSim/new-cornering-simulation/R20_with_FY.csv';
+dataPath = '/Users/Blanchards1/Documents/FormulaSim/new-cornering-simulation/R20_filtered_table.csv';
 data = readtable(dataPath);
 
 FZ = data.NormalForce;
 IA = data.InclinationAngle;
 alpha = data.SlipAngle;
 FY_exp = data.LateralForce;
-FY = data.FY;
+%FY = data.FY;
 time = data.ElapsedTime;
 
 FZ_binned = round(FZ / 50) * 50;
@@ -16,7 +16,7 @@ FZ_binned = round(FZ / 50) * 50;
 
 [alphaSorted, idx] = sort(alpha);
 FY_exp_sorted = FY_exp(idx);
-FY_calc_sorted = FY(idx);
+%FY_calc_sorted = FY(idx);
 FZ_binned_sorted = FZ_binned(idx);
 timeSorted = time(idx);
 
@@ -24,32 +24,26 @@ uniqueBins = unique(FZ_binned_sorted);
 numBins = numel(uniqueBins);
 colors = turbo(numBins);
 
-timeWindows = [13.0, 91.86; 239.92, 460.94; 716.424, 903.17];
+figure;
+hold on;
 
-for w = 1:size(timeWindows, 1)
-    tStart = timeWindows(w, 1);
-    tEnd = timeWindows(w, 2);
-
-    timeMask = (timeSorted >= tStart) & (timeSorted <= tEnd);
-
-    figure;
-    hold on;
-
-    for i = 1:numBins
-        binMask = (FZ_binned_sorted == uniqueBins(i)) & timeMask;
-        if any(binMask)
-            scatter(alphaSorted(binMask), FY_exp_sorted(binMask), 10, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'DisplayName', sprintf('Exp FY - FZ ≈ %d', uniqueBins(i)));
-        end
-    end
-
-    hold off;
-    grid on;
-    xlabel('Slip Angle');
-    ylabel('Lateral Force FY');
-
-    colormap(colors);
-    c = colorbar;
-    c.Ticks = linspace(0, 1, numBins);
-    c.TickLabels = string(uniqueBins);
-
+for i = 1:numBins
+    binMask = FZ_binned_sorted == uniqueBins(i);
+    scatter(alphaSorted(binMask), FY_exp_sorted(binMask), 10, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'DisplayName', sprintf('Exp FY - FZ ≈ %d', uniqueBins(i)));
+    %scatter(alphaSorted(binMask), FY_calc_sorted(binMask), 10, 'MarkerEdgeColor', colors(i,:), 'MarkerFaceColor', 'none', 'DisplayName', sprintf('Calc FY - FZ ≈ %d', uniqueBins(i)));
 end
+hold off;
+
+grid on;
+xlabel('Slip Angle (deg)');
+ylabel('Lateral Force FY');
+title('Experimental vs. Calculated Lateral Force Colored by Normal Force');
+legend('Location', 'bestoutside');
+
+colormap(colors);
+c = colorbar;
+c.Ticks = linspace(0, 1, numBins);
+c.TickLabels = string(uniqueBins);
+
+%rmse = sqrt(mean((FY - FY_exp).^2));
+%disp(['RMSE between experimental and calculated FY: ', num2str(rmse, '%.3f')]);
