ea8fd9b4bd5986193c939d3228b27b7cc488d971
PMurphyB <pmurphyguitar@gmail.com>
2025-11-01 12:57:42 -0600

    Began sorter

--- organize.m
+++ organize.m
@@ -5,3 +5,11 @@ dataFolder = '/Users/Blanchards1/Documents/FormulaSim/new-cornering-simulation';
 csvFiles = dir(fullfile(dataFolder, "R20_FZ_*.csv"));
 
 for i = 1:numel(csvFiles)
+    curFile = fullfile(csvFiles(i).folder, csvFiles(i).name);
+    disp("Processing" + csvFiles(i).name);
+
+    table = readtable(curFile);
+    varNames = ["RoadSpeed", "TirePressure", "InclinationAngle", "NormalForce", "SlipAngle", "ElapsedTime", "LateralForce", "Index"];
+    table = table(:, varNames);
+
+end
