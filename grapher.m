0a341e5901d42cbdefd87b7c131680d03a02de7b
PMurphyB <pmurphyguitar@gmail.com>
2025-11-01 13:28:59 -0600

    Graphing

--- grapher.m
+++ grapher.m
@@ -24,3 +24,13 @@ end
 [~, sortIdx] = sort(FZValues);
 FZValues = FZValues(sortIdx);
 colors = turbo(numel(FZValues));
+
+figure('Name', 'All FZ Combined', 'NumberTitle', 'off');
+hold on;
+for i = 1:numel(FZValues)
+    FZVal = FZValues(i);
+    table - allData(allData.FZbin == FZval, :);
+
+    scatter(table.SlipAngle, table.LateralForce, 10, 'MarkerFaceColor', colors(i,:), 'MarkerEdgeColor', 'none', 'DisplayName', sprintf('FZ = %d N', FZval));
+end
+
