cc64cbb67fb281673a3c8a6b2dde1703dac38cc8
PMurphyB <pmurphyguitar@gmail.com>
2025-11-01 12:55:24 -0600

    Restarting Loop

--- organize.m
+++ organize.m
@@ -5,13 +5,3 @@ dataFolder = '/Users/Blanchards1/Documents/FormulaSim/new-cornering-simulation';
 csvFiles = dir(fullfile(dataFolder, "R20_FZ_*.csv"));
 
 for i = 1:numel(csvFiles)
-    curFile = fullfile(dataFolder, csvFiles{i});
-    disp("Processing " + csvFiles{i});
-    table = readtable(curFile);
-    varNames = ["RoadSpeed", "TirePressure", "InclinationAngle","NormalForce", "SlipAngle", "ElapsedTime", "LateralForce", "Index"];
-    table = table(:, varNames);
-    table.RoadSpeed = round(table.RoadSpeed);
-    table.TirePressure = floor(table.TirePressure);
-    table.InclinationAngle = floor(table.InclinationAngle * 10) / 10;
-    table = sortrows(table, ["RoadSpeed", "TirePressure", "InclinationAngle"]);
-
